49f3bf318dac060f46eca602cbafb06dd1f711c3
checking6969 <0105cs221048@oriental.ac.in>
2024-05-08 14:47:24 +0530

    done

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req:Request, res:Response) => {
-		res.send("got cd")
+		res.send("got Cd and here is the dummy get request")
 });
 app.listen(3000, () => {
 	console.log('server is running and here we gos')
